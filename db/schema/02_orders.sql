c1f3496dba0edea6560a72bad8342e1dcfbc4271
hrakad <your@email.com>
2021-10-21 05:21:56 +0000

    Add orders table to schema

---
+++ db/schema/02_orders.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS orders CASCADE;
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
+  order_created_time TIMESTAMP,
+  order_start_time TIMESTAMP,
+  order_end_time TIMESTAMP
+);
