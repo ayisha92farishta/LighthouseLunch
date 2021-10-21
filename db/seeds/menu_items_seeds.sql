6a400879fd24db9079de495243548ce45a4ce2c6
Brian LaFond <brian.d.lafond@gmail.com>
2021-10-21 17:54:12 +0000

    add menu items seeds

---
+++ db/seeds/menu_items_seeds.sql
@@ -0,0 +1,56 @@
+-- SNACKS
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Yogurt Parfait', 4.99, 'description', 'https://unsplash.com/photos/M218Zx_pTLw', 60000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('French Fries', 5.99, 'description', 'https://unsplash.com/photos/BnNO5JhEz18', 180000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Smoothie Bowl', 8.99, 'description', 'https://unsplash.com/photos/Nj07arckz4Q', 120000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Donut', 2.99, 'description', 'https://unsplash.com/photos/q54Oxq44MZs', 60000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Muffin', 1.99, 'description', 'https://unsplash.com/photos/mxUf5V0-FYQ', 60000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Charcuterie', 14.99, 'description', 'https://unsplash.com/photos/r7OZ7BKOw6Q', 120000);
+
+-- DRINKS
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Coffee', 2.99, 'description', 'https://unsplash.com/photos/nBJHO6wmRWw', 60000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Cappuccino', 3.75, 'description', 'https://unsplash.com/photos/XOhI_kW_TaM', 120000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Tea', 1.99, 'description', 'https://unsplash.com/photos/Qaor6nxikUM', 60000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Juice', 2.99, 'description', 'https://unsplash.com/photos/yIE7pZUmT_s', 12000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Kombucha', 5.99, 'description', 'https://unsplash.com/photos/an5Jf8HROtY', 6000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Soda', 2.99, 'description', 'https://unsplash.com/photos/IHKR_A_THW0', 6000);
+
+-- BREAKFAST
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Breakfast Bagel', 6.99, 'description', 'https://unsplash.com/photos/kmX09a309gI', 120000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Smoked Salmon Bagel', 7.99, 'description', 'https://unsplash.com/photos/DntxQ-LNWSw', 120000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Full Breakfast', 14.99, 'description', 'https://unsplash.com/photos/WbA_PCv3_Co', 180000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Blueberry Pancakes', 8.99, 'description', 'https://unsplash.com/photos/5c_aG2FDYEI', 120000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('French Toast', 8.99, 'description', 'https://unsplash.com/photos/u00NnpVinYA', 120000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Avocado Toast', 9.99, 'description', 'https://unsplash.com/photos/xpd-3kwh6ww', 120000);
+
+-- LUNCH
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Club Sandwhich', 7.99, 'description', 'https://unsplash.com/photos/VsaXdUF-fnE', 180000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Quinoa Bowl', 9.99, 'description', 'https://unsplash.com/photos/yhc4pSbl01A', 100000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Kale Salad', 10.99, 'description', 'https://unsplash.com/photos/B-DrrO3tSbo', 100000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Burger', 12.99, 'description', 'https://unsplash.com/photos/mkVa2hLJgnI', 180000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Fried Chicken Sandwhich', 14.99, 'description', 'https://unsplash.com/photos/W6ib7FLY8cs', 180000);
+INSERT INTO menu_items (name, price, description, thumbnail_photo_url, prep_time)
+VALUES ('Soup', 6.99, 'description', 'https://unsplash.com/photos/cToNEm70cvE', 120000);
+
