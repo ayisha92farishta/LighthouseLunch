46395e28da3bfb37f39896e7f2f5d88d8a7dd4a5
hrakad <your@email.com>
2021-10-21 06:03:42 +0000

    Add order_menu_items to schema

---
+++ db/schema/04_orders_menu_items.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS order_menu_items CASCADE;
+
+CREATE TABLE order_menu_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  order_id INTEGER REFERENCES orders(id) ON DELETE CASCADE,
+  menu_items_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE
+);
