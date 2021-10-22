6a250aff903324b11cc6fb5544c41ed95a334226
Brian LaFond <brian.d.lafond@gmail.com>
2021-10-22 16:14:12 +0000

    add sendMessage function

--- twilio_test.js
+++ twilio_test.js
@@ -1,25 +1,42 @@
+const dbParams = require('./lib/db');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
-  .create({
-     body: 'TEST TEXT',
-     from: '+16042271715',
-     to: '+16135818947'
-   })
-  .then(message => console.log(message.sid));
+const sendMessage = function() {
+
+  const queryString = `
+    SELECT users.id, users.phone, orders.id
+    FROM users
+    JOIN orders ON user_id = users.id
+    WHERE orders.id = $1
+    AND users.id = $2
+  `;
+
+  const queryParams = [users.id, users.phone, orders.id];
+
+  return db.query(queryString, queryParams)
+    .then(
+      client.messages
+        .create({
+          body: `Your order number is ${orders.id}. Thank you for choosing Lighthouse Lunch!!`,
+          from: '+16042271715',
+          to: `+${users.phone}`
+        })
+        .create({
+          body: `You have a new order! Order Number: ${orders.id}.`,
+          from: '+16042271715',
+          to: '+16472343536'
+        })
+        .then(message => console.log(message.sid))
+    )
+    .then(res => res.rows[0]);
+
+};
+
+exports.sendMessage = sendMessage;
 
 
-  // const addUser = function(user) {
-  //   return pool
-  //     .query(`
-  //       INSERT INTO users (name, email, password)
-  //       VALUES($1, $2, $3)
-  //       RETURNING *
-  //       `, [user.name, user.email, user.password])
-  //     .then(res => res.rows[0]);
-  // };
-  // exports.addUser = addUser;
+
