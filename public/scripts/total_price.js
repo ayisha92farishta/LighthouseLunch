dba1d7c484ad53259bf0f2bbac542c913ab58a52
ayisha92farishta <ayisha_farishta@yahoo.com>
2021-10-27 03:42:51 +0000

    Add script file total_price.js

---
+++ public/scripts/total_price.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+  console.log("ready!");
+
+  var sumPrice = 0;
+  var tdPrice = document.getElementById('tablePrice').getElementsByTagName('td');
+
+  for (var i = 0; i < tdPrice.length; i++) {
+
+    if (tdBudget[i].className == "item-price") {
+      sumPrice += isNaN(tdPrice[i].innerHTML) ? 0 : parseInt(tdPrice[i].innerHTML);
+    }
+  }
+  document.getElementById('sumPrice').innerHTML = sumPrice;
+
+
+});
